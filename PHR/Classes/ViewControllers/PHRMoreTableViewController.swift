f44d283458b25c7fd8ff8b5028d61f1eaddcccdf
retsohuang <retsohuang@gmail.com>
2014-06-17 19:37:46 +0800

    Add new target for demo

---
+++ PHR/Classes/ViewControllers/PHRMoreTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PHRMoreTableViewController.swift
+//  PHR
+//
+//  Created by Retso Huang on 6/17/14.
+//  Copyright (c) 2014 Retso Huang. All rights reserved.
+//
+
+import UIKit
+
+class PHRMoreTableViewController: UITableViewController {
+
+  init(coder aDecoder: NSCoder!) {
+    super.init(coder: aDecoder)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  ///--------------------
+  /// @name UITableViewDelegate
+  ///--------------------
+  
+
+}
